1921b02c104d15f3ebfb9366c1b51d215006104c
Hayo van Loon <hayovanloon@gmail.com>
2019-12-27 15:38:37 +0100

    flesh out cli util

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/HayoVanLoon/naive-image-transforms
 
 go 1.13
 
-require github.com/HayoVanLoon/go-commons v0.0.0-20191226134923-8bb9a795e25f
+require (
+	cloud.google.com/go/storage v1.4.0
+	github.com/HayoVanLoon/go-commons v0.0.0-20191227133503-e8257a10d863
+)
--- main/main.go
+++ main/main.go
@@ -17,38 +17,139 @@
 package main
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/HayoVanLoon/go-commons/logjson"
 	"github.com/HayoVanLoon/naive-image-transforms"
 	"image/jpeg"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 type message struct {
-	scale    float64
-	rotation float64
+	srcUrl  string
+	scale   float64
+	rotate  float64
+	destUrl string
 }
 
-func handle(m message) {
-	f, err := os.Open("tmp/in.jpg")
-	if err != nil {
-		log.Fatal(err)
+func _close(c io.Closer) {
+	if err := c.Close(); err != nil {
+		log.Println(err.Error())
 	}
-	img, err := jpeg.Decode(f)
+}
+
+func handle(m message) error {
+	ctx := context.Background()
+
+	r, err := getReader(ctx, m.srcUrl)
 	if err != nil {
-		log.Fatal(err)
+		return nil
+	}
+	defer _close(r)
+	img, err := jpeg.Decode(r)
+	if err != nil {
+		return err
 	}
 
-	img2 := naive.Transform(img, m.scale, m.rotation)
+	img2 := naive.Transform(img, m.scale, m.rotate)
 
-	w, err := os.Create("tmp/out.jpg")
-	err = jpeg.Encode(w, img2, &jpeg.Options{
-		Quality: 100,
-	})
+	w, err := getWriter(ctx, m.destUrl)
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+	defer _close(w)
+	err = jpeg.Encode(w, img2, &jpeg.Options{Quality: 100})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func splitGsUrl(url string) (string, string, error) {
+	rem := url[5:]
+	b := strings.Index(rem, "/")
+	if b <= 0 {
+		return "", "", fmt.Errorf("malformed Google Storage url %s", url)
+	}
+	if len(rem) == b {
+		return "", "", fmt.Errorf("malformed Google Storage url %s", url)
+	}
+	return rem[:b], rem[b:], nil
+}
+
+func getReader(ctx context.Context, src string) (io.ReadCloser, error) {
+	if src[:5] == "gs://" {
+		b, o, err := splitGsUrl(src)
+		if err != nil {
+			return nil, err
+		}
+		cl, err := storage.NewClient(ctx)
+		if err != nil {
+			return nil, err
+		}
+		r, err := cl.Bucket(b).Object(o).NewReader(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return r, nil
+	} else {
+		f, err := os.Open(src)
+		if err != nil {
+			return nil, err
+		}
+		return f, nil
+	}
+}
+
+func getWriter(ctx context.Context, dest string) (io.WriteCloser, error) {
+	if dest[:5] == "gs://" {
+		b, o, err := splitGsUrl(dest)
+		if err != nil {
+			return nil, err
+		}
+		cl, err := storage.NewClient(ctx)
+		if err != nil {
+			return nil, err
+		}
+		w := cl.Bucket(b).Object(o).NewWriter(ctx)
+		return w, nil
+	} else {
+		f, err := os.Create(dest)
+		if err != nil {
+			return nil, err
+		}
+		return f, nil
 	}
 }
 
 func main() {
-	handle(message{1, 15})
+	src := flag.String("src", "", "Source image file")
+	dest := flag.String("dest", "", "Source image file")
+	scale := flag.Float64("scale", 1, "Scale by x")
+	rotate := flag.Float64("rotate", 0, "Rotate by x degrees")
+
+	if *src == "" {
+		log.Fatalln("no source file specified")
+	}
+	if *dest == "" {
+		log.Fatalln("no destination file specified")
+	}
+
+	m := message{
+		srcUrl:  *src,
+		scale:   *scale,
+		rotate:  *rotate,
+		destUrl: *dest,
+	}
+
+	err := handle(m)
+	if err != nil {
+		logjson.Critical(err.Error())
+	}
 }
